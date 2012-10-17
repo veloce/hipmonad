125f78c12ca726440e5ed802c0e76b28369eef82
Vincent Velociter <vincent.velociter@gmail.com>
2012-10-18 00:03:12 +0200

    Post bluk to hipchat

--- TwitOnHip.scala
+++ TwitOnHip.scala
@@ -1,5 +1,22 @@
+import dispatch._
+import Config._
 
 object TwitOnHip {
 
-  def main(args: Array[String]) = println("bluk!")
+  val message = "bluk bluk!"
+
+  val request = url("http://api.hipchat.com/v1/rooms/message")
+    .addQueryParameter("auth_token", token)
+    .addParameter("from", "twit on hip")
+    .addParameter("room_id", room_id)
+    .addParameter("message", message)
+    .POST
+
+  def main(args: Array[String]) {
+    val promise = Http(request OK as.String)
+    for (r <- promise) {
+      println(r)
+      Http.shutdown
+    }
+  }
 }
---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "TwitOnHip"
+
+version := "0.1"
+
+resolvers += "iliaz.com" at "http://scala.iliaz.com/"
+
+libraryDependencies += "org.scalaz" %% "scalaz-core" % "6.0.4"
+
+libraryDependencies += "com.github.ornicar" % "scalalib_2.9.1" % "2.4"
+
+libraryDependencies += "org.specs2" %% "specs2" % "1.12"
+
+libraryDependencies += "net.databinder.dispatch" %% "dispatch-core" % "0.9.2"
